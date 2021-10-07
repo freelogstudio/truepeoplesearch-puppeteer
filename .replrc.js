1ecddef6c5e92a8472b36a984a90db0cae255150
Freelog Studio LLC <freelogstudio@gmail.com>
2021-10-07 13:33:13 -0400

    update

--- .replrc.js
+++ .replrc.js
@@ -1,7 +1,7 @@
 var { TruePeopleSearch } = require('./');
 
 var scrape = async () => {
-  const page = (await TruePeopleSearch.initialize()).searchNumber('8602665407') // this number belongs to a known sex offender named Brad, so it should work
+  const page = (await TruePeopleSearch.initialize()).searchNumber('8602665407') // this number belongs to a known sex offender, so it should work
   await page.screenshot({ path: path.join(process.env.HOME, 'result.png') });
   return page;
 };
